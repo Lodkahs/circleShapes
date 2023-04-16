23efdef1881e659af1e39f1f99c1fb4467281336
Lodkahs <omalojiu@gmail.com>
2023-04-16 17:50:33 +0300

    Initial Commit

---
+++ CircularShape/CircularShapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularShapeApp.swift
+//  CircularShape
+//
+//  Created by Andrew  on 4/16/23.
+//
+
+import SwiftUI
+
+@main
+struct CircularShapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
